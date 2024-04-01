3a152c86587866687bfd781c1554e2677e35feb9
i-krishna <114757733+i-krishna@users.noreply.github.com>
2024-04-01 12:56:41 -0400

    Create WorkloadClassifier.java

---
+++ Z_WLM/WorkloadClassifier.java
@@ -0,0 +1,10 @@
+// WorkloadClassifier.java
+public class WorkloadManagerHandler {
+    public void enccreateclassify() {
+        // Logic for creating and classifying workloads
+    }
+
+    public void discon() {
+        // Logic for disconnecting from workload manager (WLM) component of Z systems
+    }
+}
